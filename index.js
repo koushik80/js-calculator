5b4a6e353fbf4868ff96545b351a08e21f75897a
Koushik Dey <koushik.dey@live.com>
2022-04-21 15:04:21 +0300

    test 11-12

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
 let trailingResult = 0;
+let opertaionOptions = ['decimal', 'divide', 'multiply', 'subtract', 'add'];
+let workingOperation = "";
 function updateDisplay(input) {
     let display = document.getElementById("display");
 
-    if (display.innerHTML === "0") {
+    if (display.innerHTML === "0" && opertaionOptions.indexOf(input) === -1) {
         display.innerHTML = input;
-    } else if (input === "add") {
-        trailingResult = display.innerHTML
+    } else if (opertaionOptions.indexOf(input) >= 0) {
+        workingOperation = input;
+        trailingResult = display.innerHTML;
         display.innerHTML = 0;
     } else if (input === "equals") {
-        display.innerHTML = parseFloat(trailingResult) + parseFloat(display.innerHTML);
+        display.innerHTML = calculate(parseFloat(trailingResult),  parseFloat(display.innerHTML), workingOperation);
         trailingResult = display.innerHTML;
     } else {
         display.innerHTML += input;
         }
 }
+
 function clearDisplay() {
     let display = document.getElementById("display");
     display.innerHTML = 0;
+}
+
+function calculate(firstNumber, secondNumber, operation) {
+    let result;
+    switch (operation) {
+      case "add":
+            result = firstNumber + secondNumber;
+        break;
+      case "subtract":
+        result = firstNumber - secondNumber;
+        break;
+      case "multiply":
+        result = firstNumber * secondNumber;
+        break;
+      case "divide":
+        result = firstNumber / secondNumber;
+        break; 
+      default:
+    
+    }
+    return result.toString();
+
 }
