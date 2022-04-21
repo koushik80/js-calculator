6c0eaa475a5d9b06210ba12a5fb3f029512225e2
Koushik Dey <koushik.dey@live.com>
2022-04-21 14:04:23 +0300

    t10 updated

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-function updateDisplay(number) {
+let trailingResult = 0;
+function updateDisplay(input) {
     let display = document.getElementById("display");
+
     if (display.innerHTML === "0") {
-        display.innerHTML = number;
+        display.innerHTML = input;
+    } else if (input === "add") {
+        trailingResult = display.innerHTML
+        display.innerHTML = 0;
+    } else if (input === "equals") {
+        display.innerHTML = parseFloat(trailingResult) + parseFloat(display.innerHTML);
+        trailingResult = display.innerHTML;
     } else {
-        display.innerHTML += number;
+        display.innerHTML += input;
         }
 }
 function clearDisplay() {
